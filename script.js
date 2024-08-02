beeb97c49d8165d54c85fedca208384c38c36569
kamedayasuchika <kame.1230yk.chika@gmail.com>
2024-08-02 19:24:05 +0900

    5週表示

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let today = new Date();
 
 const week = ["日","月","火","水","木","金","土"];
-const endOfWeek = 5;
 
 window.onload = function(){
     Calendarheader(today);
@@ -30,45 +29,45 @@ function CalendarTable(){
     thead.appendChild(thead_tr);
     table.appendChild(thead);
 
+
     var year = today.getFullYear();
     var month = today.getMonth();
     var startDayOfWeek = new Date(year, month, 1).getDay();
     var monthOfEndDay = new Date(year, month + 1, 0).getDate();
     var countDay = 0;
     var countjoin = 0;
+    var endOfWeek = 5;
 
     const tbody = document.createElement("tbody");
     tbody.id = "calendartbody";
     for(let t = 0; t < 6; t++){
         const tbody_tr = document.createElement("tr");
         for(let j = 0; j < week.length; j++){
-            //重複しているtdを書く
             if(t == 0 && startDayOfWeek == j){
-                const tbody_td = document.createElement("td");
+                const tbody_th = document.createElement("th");
                 countDay++;
                 const textday = document.createTextNode(countDay);
-                tbody_td.appendChild(textday);
-                tbody_tr.appendChild(tbody_td);
-            }else if(countDay != 0 && countDay < monthOfEndDay && t < endOfWeek){
-                const tbody_td = document.createElement("td");
+                tbody_th.appendChild(textday);
+                tbody_tr.appendChild(tbody_th);
+            }else if(countDay != 0 && countDay < monthOfEndDay && t < 5){
+                const tbody_th = document.createElement("th");
                 countDay++;
                 const textday = document.createTextNode(countDay);
-                tbody_td.appendChild(textday);
-                tbody_tr.appendChild(tbody_td);
-            }else if(t == endOfWeek && countDay < monthOfEndDay){
+                tbody_th.appendChild(textday);
+                tbody_tr.appendChild(tbody_th);
+            }else if(t == 5 && countDay < monthOfEndDay){
                 countDay++;
                 var joinweek = document.getElementById("calendartbody");  
                 var x = joinweek.rows[4];
                 var joinday = x.cells[countjoin];
                 joinday.firstChild.nodeValue = x.cells[countjoin].textContent + "/" + countDay;
                 countjoin++;
-            }else if(t < endOfWeek){ 
-                const tbody_td = document.createElement("td");
-                const textday = document.createTextNode("　");
+            }else if(t < 5){
+                const tbody_td = document.createElement("th");
+                const textday = document.createTextNode("");
                 tbody_td.appendChild(textday);
                 tbody_tr.appendChild(tbody_td);
             }
-            //重複しているappendChildを書く
             tbody.appendChild(tbody_tr);
         }
         table.appendChild(tbody);
@@ -114,3 +113,4 @@ function reset_year_month(){
     removeCalendar();
     Calendarheader(today);
 }
+
